348cf5dc656297a19bc428975f36d4b286edf88a
akbaraliievv <akbaraliev770.@gmail.com>
2022-12-02 16:35:24 +0600

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,22 @@
-import collection.Collextions;
-
-import java.awt.List;
-import java.sql.Array;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-
-//        Java 8 үчүн практикалык тапшырма
-//
-//        1. Жаңы массив тизмесин түзүү үчүн Java программасын жазыңыз,
-//        бир нече шаарларды  кошуп, аларды консольго чыгарыныз.
-        ArrayList<String> strings = new ArrayList<>();
-        strings.add("Barcelona");
-        strings.add("Milan");
-        strings.add("Almaty");
-        strings.add("Bishkek");
-        System.out.println("strings = " + strings);
-        //        2. Массив тизмесине элементти биринчи орунга киргизүү үчүн Java программасын жаз.
-        strings.add(0, "Milan");
-        System.out.println(strings);
-
-//        3. Берилген массивдердин тизмесинен элементти (белгиленген индексте) алуу үчүн Java программасын жазыңыз.
-        System.out.println(strings.get(4));
-
-//        4. Берилген элемент боюнча белгилүү бир массив элементин озгортуу үчүн Java программасын жазыңыз.
-        System.out.println(strings.set(4, "Osh"));
-
-//        5. Массив тизмесинен үчүнчү элементти очуруп салуу үчүн Java программасын жазыңыз.
-        System.out.println(strings.remove(2));
-
-//        6. Массив тизмесиндеги элементти издөө үчүн Java программасын жазыңыз.
-        System.out.println(strings.contains("Milan"));
-
-//        7. Берилген массивдердин тизмесин сорттоо үчүн Java программасын жазыңыз.
-        Collections.sort(strings);
-        System.out.println("strings = " + strings);
-
-//        8. Бир массив тизмесин экинчи масивге көчүрүү үчүн Java программасын жазыңыз.
-        ArrayList<String> strings2 = new ArrayList<>();
-        strings2.add("Naryn");
-        strings2.add("Kara-Kol");
-        strings2.add("Talas");
-        strings2.add("Osh");
-        Collections.copy(strings, strings2);
-        System.out.println("strings :" + strings);
-        System.out.println("strings2 :" + strings2);
-//
-//        9. Массив тизмесиндеги элементтерди аралаштыруу үчүн Java программасын жазып.
-//        Маанилерин консольго чыгарыныз.
-        Collections.shuffle(strings, new Random());
-        System.out.println("strings = " + strings);
-
-
-//        10. Массив тизмесиндеги элементтерди тескери өзгөртүү үчүн Java программасын жазыңыз.
-        Collections.reverse(strings);
-        System.out.println("strings = " + strings);
-
-
-//        11. Аррейлисттин бир бөлүгүн алуу үчүн Java программасын жазыңыз
-        System.out.println("strings.subList(1,3) = " + strings.subList(1, 3));
+        ArrayList<Integer>arrayList = new ArrayList<>();
+        ArrayList<Integer> evenNumbers = new ArrayList<>();
+        ArrayList<Integer> exactNumbers = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 50 ; i++) {
+            arrayList.add(random.nextInt(0,100) );
+        if(arrayList.get(i)%2 ==0){
+            evenNumbers.add(arrayList.get(i));
+        }else{
+            exactNumbers.add(arrayList.get(i));
+        }
+        }
+        System.out.println("arrayList = " + arrayList);
+        System.out.println("exactNumbers = " + exactNumbers);
+        System.out.println("evenNumbers = " + evenNumbers);
     }
 }
--- src/practiceArrayList/Practika.java
+++
@@ -1,7 +0,0 @@
-package practiceArrayList;
-
-public class Practika {
-    public static void main(String[] args) {
-
-    }
-}
